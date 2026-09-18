c5f7003d9c929580211c3a875c64850da7ada77b
Emmanuel Rojas <mamueljr@gmail.com>
2026-09-17 21:44:21 -0600

    deploy: music desde monorepo (2026-09-18 03:44)

--- finance/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/aura/finance/sw.js', { scope: '/aura/finance/' })})}
--- home/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/aura/home/sw.js', { scope: '/aura/home/' })})}
